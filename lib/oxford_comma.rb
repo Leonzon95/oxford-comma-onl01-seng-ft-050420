578f4586734a7d818a12c1d9d5dbdf5927d07050
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-06 01:25:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else 
-    string = ""
+    string = " "
     array.each do |word|
       string <<  "#{word}, "
     end
